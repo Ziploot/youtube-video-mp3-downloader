8f3b29b556ad4041f009e94397359fcf2bae9e6e
Ziploot <toridevibd@gmail.com>
2026-07-28 23:12:25 +0600

    Upload run_youtube_downloader.bat for 1-click release

---
+++ run_youtube_downloader.bat
@@ -0,0 +1,10 @@
+@echo off
+title ZipLoot YouTube Downloader Studio
+echo =======================================================
+echo   Starting ZipLoot YouTube Video ^& MP3 Downloader...
+echo =======================================================
+echo.
+echo Launching local server at http://localhost:5000...
+start http://localhost:5000
+python app.py
+pause
